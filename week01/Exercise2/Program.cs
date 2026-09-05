6876d76bd6132dd3d16f38302861577d17e870c6
Tamuno47 <tamunogodfrey@gmail.com>
2026-09-05 20:52:59 +0100

    a grade calculator feature with letter grade, pass and fail message, and signs

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,83 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        // Getting user grade percent
+        Console.Write("What is your grade percent? ");
+        string userGrade = Console.ReadLine();
+        int number = int.Parse(userGrade);
+
+        string letter = "";
+        string sign = "";
+        int lastDigit = number % 10;
+        
+        
+        if (number >= 90)
+        {
+            letter = "A";
+        }
+
+        else if (number >= 80)
+        {
+            letter = "B";
+        }
+
+        else if (number >= 70)
+        {
+            letter = "C";
+        }
+
+        else if (number >= 60)
+        {
+            letter = "D";
+        }
+
+        else
+        {
+            letter = "F";
+        }
+
+
+        if (lastDigit >= 7)
+        {
+            sign = "+";
+        }
+
+        else if (lastDigit < 3)
+        {
+            sign = "-";
+        }
+
+        else
+        {
+            sign = "";
+        }
+
+
+        // Checking if grade is + or -
+        if (number >= 93)
+        {
+            sign = "";
+        }
+
+        if (letter == "F")
+        {
+            sign = "";
+        }
+
+        // Display the result 
+        Console.WriteLine($"Your grade is: {letter}{sign}");
+
+
+        if (number >= 70)
+        {
+            Console.WriteLine("You have successfully passed the course");
+        }
+
+        else
+        {
+            Console.WriteLine("You did not pass the course, try better next time");
+        }
+
+
     }
 }
