62f64c31de1cc0fbe5346088fbd6c4a4082db20a
Tamuno47 <tamunogodfrey@gmail.com>
2026-09-05 22:56:25 +0100

    This is a number guessing game with higher and lower hint and reply option

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,45 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        // For Parts 1 and 2, where the user specified the number...
+        // Console.Write("What is the magic number? ");
+        // int magicNumber = int.Parse(Console.ReadLine());
+
+        // For Part 3, where we use a random number
+        Random randomGenerator = new Random();
+
+        string playAgain = "yes";
+
+        while (playAgain == "yes")
+        {
+            int magicNumber = randomGenerator.Next(1, 101);
+
+            int guess = -1;
+
+            while (guess != magicNumber)
+            {
+                Console.Write("What is your guess? ");
+                guess = int.Parse(Console.ReadLine());
+
+                if (magicNumber > guess)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (magicNumber < guess)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine("You guessed it!");
+                }
+            }
+
+            // Ask the user if they want to play again
+            Console.Write("Do you want to play again? ");
+            playAgain = Console.ReadLine().ToLower();
+        }
+
+        Console.WriteLine("Okay goodbye!");
     }
 }
